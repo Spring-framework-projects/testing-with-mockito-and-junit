1c8986029346cc5fd95192d444a0db10500f9acc
Luis_Rebelo <luis_rebelo@dellteam.com>
2021-05-20 17:52:53 -0300

    :tada: First commit.

---
+++ src/main/java/com/pereir4/demo/TestingWithMockitoAndJunitApplication.java
@@ -0,0 +1,13 @@
+package com.pereir4.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWithMockitoAndJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingWithMockitoAndJunitApplication.class, args);
+	}
+
+}
